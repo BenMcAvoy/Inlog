db23102c29d4448190aa3d7df7331cb90aab44ae
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-09-18 21:45:59 +0100

    Check for level and colour output

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,35 @@
 use log::{SetLoggerError, LevelFilter};
 use log::{Record, Level, Metadata};
 
+use crate::colours::colour_format;
+
+mod colours;
+
 struct SimpleLogger;
 
 impl log::Log for SimpleLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= Level::Info
+        log::set_max_level(LevelFilter::Trace);
+        metadata.level() <= Level::Trace
     }
 
     fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            println!("{} - {}", record.level(), record.args());
+        // Guard for if logger is enabled
+        if !self.enabled(record.metadata()) {
+            return;
         }
+
+        let level = record.level();
+
+        let colour = match level {
+            Level::Trace => colours::LIGHT_BLUE,
+            Level::Debug => colours::BLUE,
+            Level::Info => colours::GREEN,
+            Level::Warn => colours::YELLOW,
+            Level::Error => colours::RED,
+        };
+
+        println!("{} {}", colour_format(colour, &format!("[ {:5} ]", level)), record.args());
     }
 
     fn flush(&self) {}
@@ -19,7 +37,8 @@ impl log::Log for SimpleLogger {
 
 static LOGGER: SimpleLogger = SimpleLogger;
 
-pub fn init() -> Result<(), SetLoggerError> {
+pub fn init(filter: LevelFilter) {
     log::set_logger(&LOGGER)
-        .map(|()| log::set_max_level(LevelFilter::Info))
+        .map(|()| log::set_max_level(filter))
+        .expect("The logger couldn't be set. Quitting.");
 }
